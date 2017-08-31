cb7c9ed19ffe43f4d83686dbc3fde730aa1d7d9c
Armand Agopian <armandag@comcast.net>
2017-08-31 08:41:05 -0700

    Pr04a-Temp

---
+++ src/Pr04a-Temp-Agopian-Armand.cpp
@@ -0,0 +1,35 @@
+/*
+ * School: Diablo Valley College
+ * Term: 2017 Fall
+ * Course: ComSc-110-3120,
+ *         Introduction to Programming with C++
+ *
+ * Chapter: 04
+ * Program: Pr04a-Temp-Agopian-Armand.cpp
+ * Author: Agopian, Armand
+ * Date: August 31, 2017
+ *
+ * Purpose: Learn about arithmetic expressions with
+ *          several operations.
+ *
+ *          Consider whether to use parentheses to
+ *          override default operator precedence.
+ *
+ *          When dividing, learn to avoid truncation.
+ *
+ *          Learn to format output neatly and with
+ *          a specific number of decimal places.
+ *
+ * Practice
+ * Problem: Convert Fahrenheit temperatures shown
+ *          on the classroom screen to Celcius.
+ *
+ *          Output numeric results with one digit
+ *          to the right of a decimal point
+ *          (see textbook section 4.2).
+ *
+ * Real
+ * Problem: Do the above, plus convert additional
+ *          temperatures shown on the big screen the
+ *          other way around, from Calcius to Fahrenheit.
+ */
