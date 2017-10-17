69a73cbc278ccdf1f465f6ac83218ce2068ee844
Armand Agopian <armandag@comcast.net>
2017-10-17 09:48:22 -0700

    Pr09a-Max

---
+++ src/Pr09a-Max-Agopian-Armand.cpp
@@ -0,0 +1,49 @@
+/*
+ * School: Diablo Valley College
+ * Term: 2017 Fall
+ * Course: ComSc-110-3120,
+ *         Introduction to Programming with C++
+ *
+ * Chapter: 09
+ * Program: Pr09a-Max-Agopian-Armand.cpp
+ * Author: Agopian, Armand
+ * Date: October 17, 2017
+ *
+ * Purpose: Learn about bits, bytes, and the maximum
+ *          capacity of short integer variables.
+ *
+ *          Introduce a second level function.
+ *
+ *          Review looping, validation, conditions,
+ *          type casting, and arithmetic.
+ *
+ * Problem: Use a level one function to input,
+ *          validate, and process short integers.
+ *
+ *          Use a level two function to output program
+ *          footings that include a sum, count, and average.
+ *
+ *          Watch and listen in class, and refer to
+ *          a console document for more information.
+ *
+ *          Submit just a source program (.cpp) file.
+ */
+
+//Preprocessor Directives
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Function Prototypes
+void putHead();
+void procData();
+void calcAvg(short, short);
+
+//Main Routine
+int main()
+{
+  //Call level one functions
+  putHead();
+}
