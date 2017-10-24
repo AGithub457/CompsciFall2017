df329b16bd72792e2651c68210882d1c916948dc
Armand Agopian <armandag@comcast.net>
2017-10-24 09:21:27 -0700

    Pr10a-Elec

---
+++ src/Pr10a-Elec-Agopian-Armand.cpp.cpp
@@ -0,0 +1,24 @@
+/*
+ * School: Diablo Valley College
+ * Term: 2017 Fall
+ * Course: ComSc-110-3120,
+ *         Introduction to Programming with C++
+ *
+ * Chapter: 10
+ * Program: Pr10a-Elec-Agopian-Armand.cpp.cpp
+ * Author: Agopian, Armand
+ * Date: October 24, 2017
+ *
+ * Purpose: Learn about getting data from an input
+ *          file instead of from the console.
+ *
+ *          Review looping, validation. formatting
+ *          output, functions, and the switch-case
+ *          construct.
+ *
+ * Initial
+ * Problem: 
+ *
+ * Next
+ * Problem: 
+ */
